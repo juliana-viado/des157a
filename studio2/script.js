c9dc37aee043e0ee39dbb4cf45197ecf896bb68a
Juliana Viado <122541417+juliana-viado@users.noreply.github.com>
2023-03-02 23:55:11 -0800

    Add files via upload

---
+++ studio2/script.js
@@ -0,0 +1,23 @@
+(function(){
+    "use strict";
+    console.log('JS Running');
+
+    const headingTwo = document.querySelectorAll('card h2');
+   
+    headingTwo[0].style.color = 'black';
+    headingTwo[1].style.color = 'red';
+    headingTwo[2].style.color = 'black';
+
+    const headingThree = document.querySelectorAll('card h3');
+   
+    headingThree[0].style.color = 'black';
+    headingThree[1].style.color = 'black';
+    headingThree[2].style.color = 'red';
+    headingThree[3].style.color = 'green';
+    headingThree[4].style.color = 'blue';
+    headingThree[5].style.color = 'white';
+    headingThree[6].style.color = 'black';
+    headingThree[7].style.color = 'white';
+
+})();
+
